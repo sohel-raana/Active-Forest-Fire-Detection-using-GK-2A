6a44b861dfc49731f0e72c0bb1e8d73332c6eebb
Rana <85870833+rana-envju@users.noreply.github.com>
2024-01-09 11:09:14 +0900

    Add files via upload
    Latitude and Longitude value was extracted using this code for visualization in ArcMap 10.8

---
+++ Saving_Lat_Lon_with_CSV.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+
+# Specify the paths
+lat_lon_combined_path = '/share/wildfire-2/sohel/Project/output_csv/lat_lon_combined.csv'
+input_folder_path = '/share/wildfire-2/sohel/Project/03_Sampling/result/Wholeimage_RF/Case_num_119/trial_Final'
+output_folder_path = '/share/wildfire-2/sohel/Project/csv_latlon/119/Scheme2/new'
+
+# Read the lat_lon_combined CSV file
+lat_lon_combined = pd.read_csv(lat_lon_combined_path)
+print(lat_lon_combined)
+print("lat_lon_combined columns:", lat_lon_combined.columns)
+
+# Ensure the output folder exists; if not, create it
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
+
+# Loop through each file in the input folder
+for filename in os.listdir(input_folder_path):
+    if filename.endswith('.csv') and 'scheme2' in filename:
+        # Construct the full path of the input CSV file
+        input_file_path = os.path.join(input_folder_path, filename)
+
+        # Read the input CSV file
+        input_data = pd.read_csv(input_file_path)
+        print(f"{filename} columns:", input_data.columns)
+        print(input_data)
+
+        # Extract column name from the filename (8th to 27th index)
+        column_name = filename[8:27]  # Adjust the index range accordingly
+        print(column_name)
+        
+        # Reset index for both DataFrames
+        lat_lon_combined_reset = lat_lon_combined.reset_index(drop=True)
+        input_data_reset = input_data.reset_index(drop=True)
+        
+        # Create a temporary table with all values
+        temp_table = pd.concat([lat_lon_combined_reset, input_data_reset], axis=1)
+
+        # Filter rows based on the extracted column to keep only 1 values
+        temp_table_filtered = temp_table[temp_table[column_name] == 1]
+
+        # Construct the full path of the output CSV file with "scheme2" prefix
+        output_file_path = os.path.join(output_folder_path, f"{filename}")
+
+        # Save the filtered data to the output CSV file
+        temp_table_filtered.to_csv(output_file_path, index=False)
+
+        # Print information about the processed file
+        print(f"Processed: {filename} -> Output: {output_file_path}")
+
+print("Merging and saving completed.")
+
